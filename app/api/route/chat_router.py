00b043bce23f56c02c851213dd7ab2b3cf1c390f
inspire12 <ox4443@naver.com>
2025-12-07 19:12:04 +0900

    feat: sse (event-stream) 방식으로 데이터 전송

--- app/api/route/chat_router.py
+++ app/api/route/chat_router.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, APIRouter
-from starlette.responses import StreamingResponse
+from sse_starlette import EventSourceResponse
+# from starlette.responses import StreamingResponse
 
 from app.deps import get_chat_service
 from app.models.schemas.chat import ChatRequest
@@ -12,7 +13,9 @@ chat_router = APIRouter(prefix="/chat", tags=["chat"])
 @chat_router.post("")
 async def chat(message: ChatRequest,
                chat_service: ChatService = Depends(get_chat_service)):
-    return StreamingResponse(
-        chat_service.upstage_chat(message),
-        media_type="text/plain; charset=utf-8"
-    )
+    # return StreamingResponse(
+    #     chat_service.upstage_chat(message),
+    #     # media_type="text/plain; charset=utf-8"
+    #     media_type="text/event-stream; charset=utf-8"
+    # )
+    return  EventSourceResponse(chat_service.upstage_chat(message),)
